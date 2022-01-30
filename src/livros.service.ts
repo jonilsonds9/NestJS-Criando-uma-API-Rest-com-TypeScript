5bd22d082ede290a75dd83fdaf7cd18cfca98ff6
Jonilson Sousa <jonilsonds9@gmail.com>
2022-01-29 22:48:32 -0300

    Criando o banco de dados MySQL

---
+++ src/livros.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { Livro } from "./livro.model";
+
+@Injectable()
+export class LivrosService {
+
+    private livros: Livro[] = [
+        new Livro('LIV01', 'Livro TDD', 29.90),
+        new Livro('LIV02', 'Livro Flutter', 39.90),
+        new Livro('LIV03', 'Livro IA', 29.90)
+    ];
+
+    obterTodos(): Livro[] {
+        return this.livros;
+    }
+
+    obterUm(id: number): Livro {
+        return this.livros[0];
+    }
+
+    criar(livro: Livro) {
+        this.livros.push(livro);
+    }
+
+    alterar(livro: Livro) {
+        return livro;
+    }
+
+    apagar(id: number) {
+        this.livros.pop();
+    }
+}
